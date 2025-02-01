a52adf7a413244ec6630f7590981434f34c0e121
1Aditya7 <aditya17parthiban@gmail.com>
2025-02-01 16:07:34 +0530

    Added Vercel Analytics

--- checklist/src/app/layout.tsx
+++ checklist/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { DotPattern } from "../components/ui/dot-pattern"; // Import the DotPattern
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const geistSans = Geist({
